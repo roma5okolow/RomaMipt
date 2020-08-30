b3597b6f006dd3d7e21e0cebe8691291f28a0dd6
roma5okolow <70335780+roma5okolow@users.noreply.github.com>
2020-08-30 09:51:59 +0300

    Add files via upload

--- square equation_new.cpp
+++ square equation_new.cpp
@@ -6,8 +6,37 @@
 #define UNDEF_ROOT -1
 #define BORDER 1e-20
 
+//-----------------------------------------------------------------------------
+//! Solves a square equation ax2+bx+c = 0
+//! @param [in]  a    a - coefficient
+//! @param [in]  b    b - coefficient
+//! @param [in]  c    c - coefficient
+//! @param [out] x1   Pointer to the first root
+//! @param [out] x2   Pointer to the second root
+//!
+//! return number of roots
+//!
+//! in case of infinite number of roots,
+//! returns UNDEF_ROOT.
+//-----------------------------------------------------------------------------
+
 int Eq_Sol (double a, double b, double c,
             double* x1, double* x2);
+
+//-----------------------------------------------------------------------------
+//! Print roots of equation
+//! @param [in]  Num_Roots
+//! @param [in]  x1
+//! @param [in]  x2
+//!
+//!
+//! in case of infinite number of roots,
+//! returns UNDEF_ROOT.
+//-----------------------------------------------------------------------------
+
+
+void Solution_Checker();
+
 
 void Print_Roots (int Num_Roots, double x1, double x2);
 
@@ -15,7 +44,11 @@ void Print_Roots (int Num_Roots, double x1, double x2);
 
 int main()
     {
-    printf("Square equation solver\n)");
+
+    Solution_Checker();
+
+    return 0;
+    printf("Square equation solver\n");
     printf("Enter a, b, c: ");
 
     double a = 0, b = 0, c = 0;
@@ -38,19 +71,6 @@ int main()
     return 0;
     }
 
-//-----------------------------------------------------------------------------
-//! Solves a square equation ax2+bx+c = 0
-//! @param [in]  a    a - coefficient
-//! @param [in]  b    b - coefficient
-//! @param [in]  c    c - coefficient
-//! @param [out] x1   Pointer to the first root
-//! @param [out] x2   Pointer to the second root
-//!
-//! return number of roots
-//!
-//! in case of infinite number of roots,
-//! returns UNDEF_ROOT.
-//-----------------------------------------------------------------------------
 
 int Eq_Sol (double a, double b, double c,
             double* x1, double* x2)
@@ -99,16 +119,58 @@ int Eq_Sol (double a, double b, double c,
         }
     }
 
-//-----------------------------------------------------------------------------
-//! Print roots of equation
-//! @param [in]  Num_Roots
-//! @param [in]  x1
-//! @param [in]  x2
-//!
-//!
-//! in case of infinite number of roots,
-//! returns UNDEF_ROOT.
-//-----------------------------------------------------------------------------
+void Solution_Checker()
+    {
+
+    double x1 = 0, x2 = 0;
+
+    #define CHECKER                                                                          \
+        if ((Eq_Sol (a, b, c, &x1, &x2)) != exp_Num_Roots || x1 != exp_x1 || x2 != exp_x2)   \
+            printf("Error, false roots (a = %d, b = %d, c = %d)\n", a, b, c);                \
+        else                                                                                 \
+            {                                                                                \
+            printf("True\n");                                                                \
+            x1 = x2 = 0;                                                                     \
+            }
+
+    int    exp_Num_Roots =  2;
+    double exp_x1        = -1;
+    double exp_x2        = -2;
+    int    a             =  1;
+    int    b             =  3;
+    int    c             =  2;
+
+    CHECKER;
+
+    exp_Num_Roots =    1;
+    exp_x1        = -0.5;
+    exp_x2        = -0.5;
+    a             =    4;
+    b             =    4;
+    c             =    1;
+
+    CHECKER;
+
+    exp_Num_Roots =    0;
+    exp_x1        =    0;
+    exp_x2        =    0;
+    a             =    2;
+    b             =    1;
+    c             =    5;
+
+    CHECKER;
+
+    exp_Num_Roots =    UNDEF_ROOT;
+    exp_x1        =    0;
+    exp_x2        =    0;
+    a             =    0;
+    b             =    0;
+    c             =    0;
+
+    CHECKER;
+    }
+
+
 
 void Print_Roots (int Num_Roots, double x1, double x2)
 
